5e59cf711d15e7a0b62a3daa471cd6ae5f62599b
Batyr Nuryyev <oneturkmen@gmail.com>
2017-09-22 22:01:49 -0600

    nodejs initial

--- src/main/kotlin/edu/uwyo/cs/safemine/model/Employee.kt
+++
@@ -1,9 +0,0 @@
-package edu.uwyo.cs.safemine.model
-
-import org.springframework.data.annotation.Id
-
-data class Employee(
-        @Id val id: String,
-        val name: String,
-        val department: String?
-)
